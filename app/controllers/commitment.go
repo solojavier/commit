bee04f1a78280c57a4e7b4d7b00cff89ef667b5e
Javier Cervantes <1.27201@gmail.com>
2015-08-08 21:31:45 -0500

    Fix db route

--- app/controllers/commitment.go
+++ app/controllers/commitment.go
@@ -70,5 +70,5 @@ func parseDate(date string) time.Time {
 
 func collection(session *mgo.Session) *mgo.Collection {
 	// TODO: Add error handling
-	return session.DB("commit").C("heroku_jl9vx3v3")
+	return session.DB("heroku_jl9vx3v3").C("commitments")
 }
