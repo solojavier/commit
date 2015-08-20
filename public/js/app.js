a9c64b9b3e54cf866727bb6e5e34104adbf87177
Javier Cervantes <1.27201@gmail.com>
2015-08-20 15:29:27 -0400

    Fix some errors in javascript file

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,10 @@ $( document ).ready(function() {
 
   $("#create-button").click(function(event) {
     event.preventDefault();
-    description = $('input[name=description]').val(); // TODO: Obtener valor del form
-    date        = $('input[name=date]').val(); // TODO: Obtener del form y poner en formato esperado
+    description = $('input[name=description]').val();
+    date        = $('input[name=date]').val();
     form = { description: description, date: date };
-    $.post("/commitment/" + Commit.user, function(data) {
+    $.post("/commitment/" + Commit.user, form, function(data) {
       console.log(form);
       retrieveCommit();
     });
@@ -40,21 +40,16 @@ var retrieveCommit = function() {
     $("#create-form").hide();
     Commit.id = data.id;
 
-    
-
-    $( "#description" ).text( form.description );
-    
-    var aux = form.date.split("-");
+    var aux = data.date.split("-");
     var year = aux[0];
     var month = aux[1];
     var day = aux[2];
+
     $( "#day" ).text( day );
     $( "#month" ).text( month );
     $( "#year" ).text( year );
+    $( "#description" ).text( data.description );
 
- 
-    //$("#date").text( "data.date" );
-    //TODO: Llenar los valores de description y date en la interfaz con data.description y data.date
     $("#show-form").show();
   }).fail(function() {
     $("#create-form").show();
