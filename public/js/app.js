c09a03cd4ae09d8536dd6463c1752c66765437b0
OctavioRazo <octavio.razo.verduzco@gmail.com>
2015-08-20 11:55:27 -0500

    instruciones de comentarios /TODO

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,11 @@ $( document ).ready(function() {
 
   $("#create-button").click(function(event) {
     event.preventDefault();
-    description = "description"; // TODO: Obtener valor del form
-    date        = ""; // TODO: Obtener del form y poner en formato esperado
-    form = { description: description, date: date }
+    description = $('input[name=description]').val(); // TODO: Obtener valor del form
+    date        = $('input[name=date]').val(); // TODO: Obtener del form y poner en formato esperado
+    form = { description: description, date: date };
     $.post("/commitment/" + Commit.user, function(data) {
+      console.log(form);
       retrieveCommit();
     });
   });
@@ -38,6 +39,21 @@ var retrieveCommit = function() {
   $.get("/commitment/" + Commit.user, function(data) {
     $("#create-form").hide();
     Commit.id = data.id;
+
+    
+
+    $( "#description" ).text( form.description );
+    
+    var aux = form.date.split("-");
+    var year = aux[0];
+    var month = aux[1];
+    var day = aux[2];
+    $( "#day" ).text( day );
+    $( "#month" ).text( month );
+    $( "#year" ).text( year );
+
+ 
+    //$("#date").text( "data.date" );
     //TODO: Llenar los valores de description y date en la interfaz con data.description y data.date
     $("#show-form").show();
   }).fail(function() {
